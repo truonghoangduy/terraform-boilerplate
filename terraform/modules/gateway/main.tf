0550daaf3cab9f27b70653086cbcf41e8f38e304
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-02-08 14:50:12 +0700

    refactor gateway and user openapi standard

--- terraform/modules/gateway/main.tf
+++ terraform/modules/gateway/main.tf
@@ -1,56 +1,52 @@
+data "template_file" "config_swagger_gateway_lambda" {
+  template = file("${path.module}/course-app-swagger-apigateway.yaml")
+
+  vars = {
+    course_list_title_get_lambda_arn  = var.course_list_title_get_lambda_arn
+    course_detail_get_lambda_arn      = var.course_detail_get_lambda_arn
+    course_option_lambda_arn          = var.course_option_lambda_arn
+    course_get_lambda_arn             = var.course_get_lambda_arn
+    user_course_post_lambda_arn       = var.user_course_post_lambda_arn
+    user_course_get_lambda_arn        = var.user_course_get_lambda_arn
+    user_course_detail_put_lambda_arn = var.user_course_detail_put_lambda_arn
+    user_course_detail_get_lambda_arn = var.user_course_detail_get_lambda_arn
+  }
+}
+
 resource "aws_api_gateway_rest_api" "course_app_api_gateway" {
-  name = "course-app-api-gateway"
+  name = var.aws_api_gateway_rest_api_name
+
+  description = "course app REST API"
+  body        = data.template_file.config_swagger_gateway_lambda.rendered
+
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 
 }
 
-resource "aws_api_gateway_resource" "course" {
-  rest_api_id = aws_api_gateway_rest_api.course_app_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.course_app_api_gateway.root_resource_id
-  path_part   = "course"
-}
-
-resource "aws_api_gateway_method" "get" {
-  rest_api_id      = aws_api_gateway_rest_api.course_app_api_gateway.id
-  resource_id      = aws_api_gateway_resource.course.id
-  http_method      = "GET"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_integration" "course_get_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.course_app_api_gateway.id
-  resource_id             = aws_api_gateway_resource.course.id
-  http_method             = aws_api_gateway_method.get.http_method
-  integration_http_method = "POST"
-  // MOCK
-  type = "AWS_PROXY"
-  uri  = var.course_get_lambda_arn.invoke_arn
-}
-
-
-
 resource "aws_api_gateway_deployment" "course_app_get_deployment" {
   rest_api_id = aws_api_gateway_rest_api.course_app_api_gateway.id
-  depends_on = [
-    aws_api_gateway_integration.course_get_integration
-  ]
+  # triggers = {
+  #   re
+  # }
+
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = var.course_get_lambda_arn.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.course_app_api_gateway.execution_arn}/*/*"
-  depends_on = [
-    aws_api_gateway_rest_api.course_app_api_gateway
-  ]
+# resource "aws_lambda_permission" "apigw" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = var.course_get_lambda_arn.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_api_gateway_rest_api.course_app_api_gateway.execution_arn}/*/*"
+#   depends_on = [
+#     aws_api_gateway_rest_api.course_app_api_gateway
+#   ]
+# }
+
+output "gateway_url" {
+  value = aws_api_gateway_deployment.course_app_get_deployment.invoke_url
 }
-
-
 
 output "aws_api_gateway_rest_api" {
   value = aws_api_gateway_rest_api.course_app_api_gateway
--- terraform/modules/gateway/variable.tf
+++ terraform/modules/gateway/variable.tf
@@ -1,4 +1,35 @@
+variable "aws_api_gateway_rest_api_name" {
+  type    = string
+  default = "course-app-api-gateway-terraform"
+}
+variable "course_list_title_get_lambda_arn" {
+  description = "course_list_title_get_lambda_arn"
+}
+
+variable "course_detail_get_lambda_arn" {
+  description = "course_detail_get_lambda_arn"
+}
+
+variable "course_option_lambda_arn" {
+  description = "course_option_lambda_arn"
+}
+
 variable "course_get_lambda_arn" {
   description = "course_get_lambda_arn"
 }
 
+variable "user_course_post_lambda_arn" {
+  description = "user_course_post_lambda_arn"
+}
+
+variable "user_course_get_lambda_arn" {
+  description = "user_course_get_lambda_arn"
+}
+
+variable "user_course_detail_put_lambda_arn" {
+  description = "user_course_detail_put_lambda_arn"
+}
+
+variable "user_course_detail_get_lambda_arn" {
+  description = "user_course_detail_get_lambda_arn"
+}
